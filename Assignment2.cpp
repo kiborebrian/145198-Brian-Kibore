b02ca1c931ae298b0ce77e4fab1e2375c945c342
Brian <kiborebrian@gmail.com>
2023-10-09 17:35:46 +0300

    first commit

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    cout<<"   o    0              ______                    "<<endl;
+cout<<"   o    0              ______                        "<<endl;
 cout<<"  /|\\  /8\    _______/_\\____                       "<<endl; 
  cout<<"   |   /_\\     ||__--____--____|                   "<<endl; 
 cout<<" / \\ |          ()    ()                            "<<endl;   
                                              
 cout<<" Stick man and woman holding hands                   "<<endl;   
                                              
-cout<<"  next to their car  "<<endl;
+cout<<"  next to their car  "                                <<endl;
 
 }
