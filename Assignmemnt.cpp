6b894bd15105bd8ec61734058155836bfbbe7cc8
Brian <kiborebrian@gmail.com>
2023-10-09 17:09:20 +0300

    git add Assignment 2.cpp git commit

---
+++ Assignmemnt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Welcome to the wonderful world of C++!\n";
+    cout<<"This is my first C++ output statement\n";
+    cout<<"Soon, I will learn how to input data into my program\n";
+
+}
---
+++ Helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World. This is my first C++ program\n";
+    cout<<"I am very excited and ready to learn C++\n";
+    cout<<"This is a great University\n";
+}
