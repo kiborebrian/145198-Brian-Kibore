81cf1ee7d968f134d75f983cdeda221d5cb918ea
Brian <kiborebrian@gmail.com>
2023-10-09 17:13:41 +0300

    git add Assignment 2.cpp

---
+++ .vscode/Assignment 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"   o    0              ______                    "<<endl;
+cout<<"  /|\\  /8\    _______/_\\____                       "<<endl; 
+ cout<<"   |   /_\\     ||__--____--____|                   "<<endl; 
+cout<<" / \\ |          ()    ()                            "<<endl;   
+                                             
+cout<<" Stick man and woman holding hands                   "<<endl;   
+                                             
+cout<<"  next to their car  "<<endl;
+
+}
